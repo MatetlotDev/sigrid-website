b1c5401cd5fda3d0c7ac41a008bc01e8bfb15e05
Matthias Lechien <matthias.lechien@famoco.com>
2023-01-09 08:46:17 +0100

    create random pictures for log in

---
+++ src/constants.js
@@ -0,0 +1,183 @@
+export const availablePictures = [
+  {
+    id: 1,
+    name: 'Stillness',
+    dimensions: '1520x1210',
+    path: './public/images/available/available000.jpeg',
+    price: 3500,
+  },
+  {
+    id: 2,
+    name: 'Misty day',
+    dimensions: '300x910',
+    path: './public/images/available/available004.jpeg',
+    price: 600,
+  },
+  {
+    id: 3,
+    name: 'Flax',
+    dimensions: '550x550',
+    path: './public/images/available/available003.jpeg',
+    price: 650,
+  },
+  {
+    id: 4,
+    name: 'Deep ocean',
+    dimensions: '760x760',
+    path: './public/images/available/available001.jpeg',
+    price: 950,
+  },
+  {
+    id: 5,
+    name: 'Eucalyptus',
+    dimensions: '760x1020',
+    path: './public/images/available/available009.jpeg',
+    price: 1100,
+  },
+  {
+    id: 6,
+    name: 'Voyage',
+    dimensions: '500x500',
+    path: './public/images/available/available005.jpeg',
+    price: 750,
+  },
+  {
+    id: 7,
+    name: 'Forest',
+    dimensions: '355x455',
+    path: './public/images/available/available008.jpeg',
+    price: 650,
+  },
+  {
+    id: 8,
+    name: 'NZ rock wren',
+    dimensions: '500x500',
+    path: './public/images/available/available006.jpeg',
+    price: 850,
+  },
+  {
+    id: 9,
+    name: 'Organic light',
+    dimensions: '760x760',
+    path: './public/images/available/available002.jpeg',
+    price: 1200,
+  },
+  {
+    id: 10,
+    name: 'Deep mountain',
+    dimensions: '450x450',
+    path: './public/images/available/available014.jpeg',
+    price: 550,
+  },
+  {
+    id: 11,
+    name: 'Vibrancy',
+    dimensions: '350x400',
+    path: './public/images/available/available015.jpg',
+    price: 850,
+  },
+  {
+    id: 12,
+    name: 'Beginnings',
+    dimensions: '760x1020',
+    path: './public/images/available/available010.jpeg',
+    price: 1800,
+  },
+  {
+    id: 13,
+    name: 'Pīwakawaka',
+    dimensions: '355x455',
+    path: './public/images/available/available007.jpeg',
+    price: 550,
+  },
+  {
+    id: 14,
+    name: 'Earth',
+    dimensions: '760x760',
+    path: './public/images/available/available011.jpeg',
+    price: 1600,
+  },
+];
+
+export const notAvailablePictures = [
+  {
+    id: 15,
+    path: './public/images/not-available/available000.jpeg',
+  },
+  {
+    id: 16,
+    path: './public/images/not-available/available001.jpeg',
+  },
+  {
+    id: 17,
+    path: './public/images/not-available/available002.jpeg',
+  },
+  {
+    id: 18,
+    path: './public/images/not-available/available003.jpeg',
+  },
+  {
+    id: 19,
+    path: './public/images/not-available/available004.jpeg',
+  },
+  {
+    id: 20,
+    path: './public/images/not-available/available005.jpeg',
+  },
+  {
+    id: 21,
+    path: './public/images/not-available/available006.jpeg',
+  },
+  {
+    id: 22,
+    path: './public/images/not-available/available007.jpeg',
+  },
+  {
+    id: 23,
+    path: './public/images/not-available/available008.jpeg',
+  },
+  {
+    id: 24,
+    path: './public/images/not-available/available009.jpeg',
+  },
+  {
+    id: 25,
+    path: './public/images/not-available/available010.jpeg',
+  },
+  {
+    id: 26,
+    path: './public/images/not-available/available011.jpeg',
+  },
+  {
+    id: 27,
+    path: './public/images/not-available/available012.jpeg',
+  },
+  {
+    id: 28,
+    path: './public/images/not-available/available013.jpeg',
+  },
+  {
+    id: 29,
+    path: './public/images/not-available/available014.jpeg',
+  },
+  {
+    id: 30,
+    path: './public/images/not-available/available015.jpeg',
+  },
+  {
+    id: 31,
+    path: './public/images/not-available/available016.jpeg',
+  },
+  {
+    id: 32,
+    path: './public/images/not-available/available017.jpeg',
+  },
+  {
+    id: 33,
+    path: './public/images/not-available/available018.jpeg',
+  },
+  {
+    id: 34,
+    path: './public/images/not-available/available019.jpeg',
+  },
+];
--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,7 +1,27 @@
+import styled from 'styled-components';
 import Header from '../../components/Header';
 import Quote from '../../components/Quote';
+import { availablePictures, notAvailablePictures } from '../../constants';
+
+const Pictures = styled.section``;
+const PicturesLine = styled.div``;
+const Button = styled.button``;
+const ButtonWrapper = styled.div``;
+const Image = styled.img``;
 
 function Home() {
+  const randomFolder =
+    Math.random() > 0.5 ? notAvailablePictures : availablePictures;
+  const randomPictures = [randomFolder[Math.abs(Math.round(Math.random() * randomFolder.length))].id];
+
+  while (randomPictures.length < 4) {
+    const fold = Math.random() > 0.5 ? notAvailablePictures : availablePictures;
+    const num = fold[Math.abs(Math.round(Math.random() * fold.length - 1))].id;
+    if (!randomPictures.includes(num)) randomPictures.push(fold[num].id);
+  }
+
+  console.log(randomPictures);
+
   return (
     <>
       <Header />
@@ -12,6 +32,13 @@ function Home() {
           unique key to unlock the knowledge of the hidden world.
         </p>
       </Quote>
+      <Pictures>
+        <PicturesLine></PicturesLine>
+        <ButtonWrapper>
+          <Button outlined>discover</Button>
+        </ButtonWrapper>
+        <PicturesLine>{/* 4 random pictures */}</PicturesLine>
+      </Pictures>
     </>
   );
 }
