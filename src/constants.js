6a2da69fb4180657e8feda151226f1aeb9b97236
Matthias Lechien <matthias.lechien@famoco.com>
2024-09-08 22:43:16 +0200

    style: add new images

--- src/constants.js
+++ src/constants.js
@@ -1,179 +1,65 @@
 import { v4 as uuid } from 'uuid';
 
-export const notAvailablePictures = [
+export const allPictures = [
   {
     id: uuid(),
-    name: 'Stillness',
-    dimensions: '1520x1210',
-    path: '/images/not_available/not_available000.webp',
-    price: 3500,
+    path: '/images/image_001.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available001.webp',
+    path: '/images/image_002.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available002.webp',
+    path: '/images/image_003.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available003.webp',
+    path: '/images/image_004.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available004.webp',
+    path: '/images/image_005.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available005.webp',
+    path: '/images/image_006.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available006.webp',
+    path: '/images/image_007.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available007.webp',
+    path: '/images/image_008.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available008.webp',
+    path: '/images/image_009.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available009.webp',
+    path: '/images/image_010.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available010.webp',
+    path: '/images/image_011.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available011.webp',
+    path: '/images/image_012.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available012.webp',
+    path: '/images/image_013.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available013.webp',
+    path: '/images/image_014.webp',
   },
   {
     id: uuid(),
-    path: '/images/not_available/not_available014.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available15.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available015.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available016.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available017.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available018.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available022.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available023.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available043.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available066.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available075.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available0123.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available0129.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available0156.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available173.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available0198.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available0231.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available0234.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available238.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available0983.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available01382.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available02312.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available02342.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available2384.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available2736.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available2912.webp',
-  },
-  {
-    id: uuid(),
-    path: '/images/not_available/not_available2983.webp',
-  },
-];
-
-export const availablePictures = [
-  {
-    id: uuid(),
-    path: '/images/available/available2329.webp',
+    path: '/images/available2329.webp',
     name: 'Earth',
     dimensions: '760x760',
     price: 1600,
@@ -182,56 +68,261 @@ export const availablePictures = [
     id: uuid(),
     name: 'Flax',
     dimensions: '550x550',
-    path: '/images/available/available003.webp',
+    path: '/images/available003.webp',
     price: 650,
   },
   {
     id: uuid(),
     name: 'Deep ocean',
     dimensions: '760x760',
-    path: '/images/available/available001.webp',
+    path: '/images/available001.webp',
     price: 950,
   },
   {
     id: uuid(),
     name: 'Eucalyptus',
     dimensions: '760x1020',
-    path: '/images/available/available009.webp',
+    path: '/images/available009.webp',
     price: 1100,
   },
   {
     id: uuid(),
     name: 'Deep mountain',
     dimensions: '450x450',
-    path: '/images/available/available014.webp',
+    path: '/images/available014.webp',
     price: 550,
   },
   {
     id: uuid(),
     name: 'Pīwakawaka',
     dimensions: '355x455',
-    path: '/images/available/available007.webp',
+    path: '/images/available007.webp',
     price: 550,
   },
   {
     id: uuid(),
-    path: '/images/available/available012.webp',
+    path: '/images/available012.webp',
   },
   {
     id: uuid(),
-    path: '/images/available/available0129.webp',
+    path: '/images/available0129.webp',
   },
   {
     id: uuid(),
-    path: '/images/available/available923.webp',
+    path: '/images/available923.webp',
   },
   {
     id: uuid(),
-    path: '/images/available/available9873.webp',
+    path: '/images/available9873.webp',
+  },
+  {
+    id: uuid(),
+    name: 'Stillness',
+    dimensions: '1520x1210',
+    path: '/images/not_available000.webp',
+    price: 3500,
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available001.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available002.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available003.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available004.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available005.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available006.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available007.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available008.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available009.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available010.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available011.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available012.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available013.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available014.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available15.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available015.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available016.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available017.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available018.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available022.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available023.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available043.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available066.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available075.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available0123.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available0129.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available0156.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available173.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available0198.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available0231.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available0234.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available238.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available0983.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available01382.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available02312.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available02342.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available2384.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available2736.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available2912.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/not_available2983.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/image_015.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/image_016.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/image_017.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/image_018.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/image_019.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/image_020.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/image_021.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/image_022.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/image_023.webp',
+  },
+  {
+    id: uuid(),
+    path: '/images/image_024.webp',
   },
 ];
-
-export const allPictures = availablePictures.concat(notAvailablePictures);
 
 // contact form errors
 const nameError = 'Your first name should be entered so I know who you are.';
